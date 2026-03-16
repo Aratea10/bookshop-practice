b8fef3970941e4ebe5e52ceebe8a23f57344a851
Aratea <saragallegomendez10@gmail.com>
2026-03-16 19:01:48 +0100

    refactor: remove legacy Book files replaced by hexagonal architecture

--- src/books/my-books.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { getMyBooksController } from './book.controller.js';
-import { authMiddleware } from '../shared/middlewares/auth.middleware.js';
-
-const myBooksRouter = Router();
-
-myBooksRouter.get('/books', [authMiddleware], getMyBooksController);
-
-export default myBooksRouter;
